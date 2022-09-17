0d601ff71d504d273aa78fca7e326410fbc26a24
Andreas Köttl <a.koettl@hotmail.com>
2022-09-17 16:02:32 +0200

    updated database script with all tables needed

--- createDatabase.sh
+++ createDatabase.sh
@@ -1,14 +1,41 @@
 #!/bin/sh
 
-# Drop and create database.
-sudo mysql -e "DROP DATABASE IM510_Project;"
-sudo mysql -e "CREATE DATABASE IM510_Project;"
+sudo mysql -e "
+DROP DATABASE IM510_Project;
+CREATE DATABASE IM510_Project;
 
-# Create tables.
-sudo mysql -e "CREATE TABLE IM510_Project.test (username text NOT NULL, password text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+CREATE TABLE IM510_Project.topic (
+    id INT(20) NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(255) NOT NULL, 
+    users__id INT(20) NOT NULL, 
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-# Drop and create user.
-sudo mysql -e "DROP USER IM510@localhost;"
-sudo mysql -e "CREATE USER IM510@localhost IDENTIFIED BY 'IM510';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO IM510@localhost;"
-sudo mysql -e "GRANT ALL PRIVILEGES ON IM510_Project.test TO IM510@localhost;"
+CREATE TABLE IM510_Project.thread (
+    id INT(20) NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(255) NOT NULL, 
+    users__id INT(20) NOT NULL, 
+    topic__id INT(20) NOT NULL, 
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IM510_Project.post (
+    id INT(20) NOT NULL AUTO_INCREMENT,
+    text TEXT NOT NULL,
+    users__id INT(20) NOT NULL, 
+    thread__id INT(20) NOT NULL, 
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IM510_Project.users (
+    id INT(20) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP USER IM510@localhost;
+CREATE USER IM510@localhost IDENTIFIED BY 'IM510';
+
+GRANT ALL PRIVILEGES ON IM510_Project.* TO IM510@localhost;"

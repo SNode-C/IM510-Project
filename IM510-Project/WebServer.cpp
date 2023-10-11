ddd1ae58bdaee1f253182615462c365a09eb6869
Volker Christian <me@vchrist.at>
2023-10-11 07:28:37 +0200

    Follow Framework API change of listen and connect

--- IM510-Project/WebServer.cpp
+++ IM510-Project/WebServer.cpp
@@ -2,49 +2,51 @@
 
 #include "WebServer.h"
 
-WebServer::WebServer(int argc, char* argv[])
-{
-    express::legacy::in::WebApp::init(argc, argv);
+WebServer::WebServer(int argc, char *argv[]) {
+  express::legacy::in::WebApp::init(argc, argv);
 
-    webApp = express::legacy::in::WebApp("legacy");
+  webApp = express::legacy::in::WebApp("legacy");
 
-    initRoutes();
-    listen();
+  initRoutes();
+  listen();
 }
 
-int WebServer::start()
-{
-    return express::WebApp::start();
+int WebServer::start() { return express::WebApp::start(); }
+
+bool WebServer::isLoggedIn() { return loggedInAs.length() > 0; }
+
+std::string WebServer::getLoggedInUserName() { return loggedInAs; }
+
+void WebServer::setLoggedInUserName(std::string username) {
+  loggedInAs = username;
 }
 
-bool WebServer::isLoggedIn()
-{
-    return loggedInAs.length() > 0;
+void WebServer::initRoutes() {
+  forumController.initRoutes(*this);
+  userController.initRoutes(*this);
+  postController.initRoutes(*this);
+  threadController.initRoutes(*this);
+  topicController.initRoutes(*this);
 }
 
-std::string WebServer::getLoggedInUserName()
-{
-    return loggedInAs;
-}
-
-void WebServer::setLoggedInUserName(std::string username)
-{
-    loggedInAs = username;
-}
-
-void WebServer::initRoutes()
-{
-    forumController.initRoutes(*this);
-    userController.initRoutes(*this);
-    postController.initRoutes(*this);
-    threadController.initRoutes(*this);
-    topicController.initRoutes(*this);
-}
-
-void WebServer::listen()
-{
-    webApp.listen([](const core::ProgressLog& progressLog) -> void {
-            progressLog.logProgress();
+void WebServer::listen() {
+  using SocketAddress = express::legacy::in::WebApp::SocketAddress;
+  webApp.listen(
+      [](const SocketAddress &socketAddress, const core::socket::State &state)
+          -> void { // Listen on all IPv4 interfaces on port 8001{
+        switch (state) {
+        case core::socket::State::OK:
+          VLOG(1) << "in: listening on '" << socketAddress.toString() << "'";
+          break;
+        case core::socket::State::DISABLED:
+          VLOG(1) << "in: disabled";
+          break;
+        case core::socket::State::ERROR:
+          VLOG(1) << "in: non critical error occurred";
+          break;
+        case core::socket::State::FATAL:
+          VLOG(1) << "in: critical error occurred";
+          break;
         }
-    );
+      });
 }
